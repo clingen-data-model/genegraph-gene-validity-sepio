cafd6468f06e6f24b0f26b2cb47761bc5f9f5b22
Tristan Nelson <thnelson@geisinger.edu>
2026-03-14 13:29:16 -0400

    Updated framework

--- src/genegraph/transform/gene_validity.clj
+++ src/genegraph/transform/gene_validity.clj
@@ -13,7 +13,6 @@
             [genegraph.framework.storage :as storage]
             [io.pedestal.interceptor :as interceptor]
             [io.pedestal.log :as log]
-            [io.pedestal.http :as http]
             [clojure.java.io :as io]
             [clojure.set :as set])
   (:import [java.time Instant])
@@ -246,20 +245,14 @@
   {:gene-validity-server
    {:type :http-server
     :name :gv-ready-server
-    ::http/host "0.0.0.0"
-    ::http/allowed-origins {:allowed-origins (constantly true)
-                            :creds true}
-    ::http/routes
+    :routes
     [["/ready"
       :get (fn [_] {:status 200 :body "server is ready"})
       :route-name ::readiness]
      ["/live"
       :get (fn [_] {:status 200 :body "server is live"})
       :route-name ::liveness]]
-    ::http/type :jetty
-    ::http/port 8888
-    ::http/join? false
-    ::http/secure-headers nil}})
+    :port 8888}})
 
 (def gv-transformer-def
   {:type :genegraph-app
