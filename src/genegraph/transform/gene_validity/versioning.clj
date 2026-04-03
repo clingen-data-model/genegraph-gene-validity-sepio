612ac38bee25d19ae238f2d6fb6be3c5bea1082b
Tristan Nelson <thnelson@geisinger.edu>
2026-04-03 13:33:18 -0400

    Removed old code from versioning

--- src/genegraph/transform/gene_validity/versioning.clj
+++ src/genegraph/transform/gene_validity/versioning.clj
@@ -1,42 +1,11 @@
 (ns genegraph.transform.gene-validity.versioning
   (:require [genegraph.framework.event :as event]
-            [genegraph.framework.storage :as storage]
             [genegraph.framework.storage.rdf :as rdf]
-            [genegraph.transform.gene-validity.proposition :as proposition]
-            [genegraph.transform.gene-validity.changes :as changes]
             [genegraph.transform.gene-validity.abbreviate :as abbrev]
             [io.pedestal.interceptor :as interceptor]
             [io.pedestal.log :as log]
             [clojure.set :as set])
   (:import [java.time Instant]))
-
-(def activity-with-role
-  (rdf/create-query "select ?activity where
-{ ?activity :cg/activityType ?role }"))
-
-(def curation-reasons
-  (rdf/create-query "select ?reasons where
-{ ?curation :cg/curationReasons ?reasons }"))
-
-(def publish-actions
-  (rdf/create-query "select ?x where { ?x :cg/activityType :cg/Submitted } "))
-
-(defn has-publish-action [m]
-  (seq (publish-actions m)))
-
-(defn approval-date [model]
-  (some-> (activity-with-role model {:role :cg/Submitted})
-          first
-          (rdf/ld1-> [:cg/date])))
-
-(defn event->approval-ms [event]
-  (-> event
-      :gene-validity/approval-date
-      Instant/parse
-      .toEpochMilli))
-
-(def six-months
-  (* 1000 60 60 24 30 6))
 
 (def recuration-reasons
   #{:cg/RecurationCommunityRequest
@@ -46,62 +15,6 @@
     :cg/RecurationErrorAffectingScoreorClassification
     :cg/RecurationDiscrepancyResolution})
 
-(defn recuration-from-gci-reasons? [event]
-  (let [reasons (set (map rdf/->kw
-                          (curation-reasons
-                           (:gene-validity/model event))))]
-    (if (seq reasons)
-      (if (seq (set/intersection recuration-reasons reasons))
-        :recuration
-        :no-recuration)
-      :no-gci-reasons)))
-
-(defn estimated-recuration? [event prior-event]
-  (let [event-time (event->approval-ms event)
-        prior-time (event->approval-ms prior-event)]
-    (< six-months (- event-time prior-time))))
-
-(defn recuration? [event prior-event]
-  (let [gci-recuration (recuration-from-gci-reasons? event)]
-    (or (= :recuration gci-recuration)
-        (and (= :no-gci-reasons gci-recuration)
-             (estimated-recuration? event prior-event)))))
-
-(defn add-change-type [event prior-event]
-  (assoc event
-         :gene-validity/change-type
-         (cond
-           (recuration? event prior-event) :major-change
-           (changes/no-change? event prior-event) :no-change
-           :default :minor-change)))
-
-(defn add-version-increment-given-change [event prior-event]
-  (let [prior-version (:gene-validity/version prior-event)]
-    (assoc event
-           :gene-validity/version
-           (case (:gene-validity/change-type event)
-             :no-change prior-version
-             :major-change {:major (inc (:major prior-version)) :minor 0}
-             (update prior-version :minor inc)))))
-
-(defn calculate-version-given-prior-version [event prior-version]
-  (-> event
-      (add-change-type prior-version)
-      (add-version-increment-given-change prior-version)))
-
-(defn add-version-map [event]
-  (if-let [prior-version (:gene-validity/previous-model event)]
-    (-> event
-        (changes/add-changes prior-version)
-        (calculate-version-given-prior-version prior-version))
-    (assoc event
-           :gene-validity/version {:major 1 :minor 0}
-           :gene-validity/change-type :new-curation)))
-
-(defn add-approval-date [event]
-  (assoc event
-         :gene-validity/approval-date
-         (approval-date (:gene-validity/model event))))
 
 (def assertion-iri
   (rdf/create-query "select ?x where { ?x a :cg/Statement }"))
@@ -154,28 +67,6 @@ construct {
                                        :sequence sequence}))))
 
 
-(defn add-prop-iri [event]
-  (assoc event
-         ::proposition-iri
-         (-> event
-             :gene-validity/model
-             proposition/prop-query
-             first
-             str)))
-
-#_(defn calculate-version [event]
-  (let [event-with-approval-date (add-approval-date event)]
-    (if (and (has-publish-action (:gene-validity/model event))
-             (:gene-validity/approval-date event-with-approval-date))
-      (-> event-with-approval-date
-          add-prop-iri
-          add-version-map
-          add-versioned-model)
-      event-with-approval-date)))
-
-#_(defn update-unpublish-event [event]
-  (let [prior-version (read-prior-version )]))
-
 (defn first-curation? [event]
   (nil? (:gene-validity/last-outcome event)))
 
@@ -203,6 +94,9 @@ construct {
       Instant/parse
       .toEpochMilli))
 
+(def six-months
+  (* 1000 60 60 24 30 6))
+
 (defn calculated-curation-approval-time-change? [event]
   (< six-months
      (- (event->approval-ms event)
@@ -220,9 +114,6 @@ construct {
      (gci-minor-change? event) :gci-minor-change
      (calculated-curation-approval-time-change? event) :time-delta-recuration
      :else :default-minor-change)))
-
-(defn inc-version [version change-level]
-  (update version change-level inc))
 
 (defn inc-major [{:keys [major]}]
   {:major (inc major)
